6008ce5a5f3ebc2bcf14033f80bbfb8fb819ac87
Frank S. Thomas <frank@timepit.eu>
2015-03-19 23:49:27 +0100

    Make Deque final

--- core/src/main/scala/purefun/Deque.scala
+++ core/src/main/scala/purefun/Deque.scala
@@ -1,54 +1,72 @@
 package purefun
 
-class Deque[A](private val front: StrictList[A], private val rear: StrictList[A]) {
+final class Deque[A] private (front: StrictList[A], rear: StrictList[A]) {
   import Deque._
   import StrictList._
 
-  private def balance: Deque[A] =
-    (front, rear) match {
-      case (Nil(), Nil()) => this
-      case (f, Nil()) => fromStrictList(f)
-      case (Nil(), r) =>
-        val (nr, nf) = r.halve
-        new Deque(nf.reverse, nr)
-      case _ => this
-    }
+  def +:(a: A): Deque[A] =
+    balance(a :: front, rear)
 
-  final def headOption: Option[A] =
+  def :+(a: A): Deque[A] =
+    balance(front, a :: rear)
+
+  def foreach(f: A => Unit): Unit = {
+    front.foreach(f)
+    rear.reverse.foreach(f)
+  }
+
+  def headOption: Option[A] =
     front.headOption
 
-  final def initOption: Option[Deque[A]] =
+  def initOption: Option[Deque[A]] =
     rear match {
-      case Cons(h, t) => Some(new Deque(front, t).balance)
+      case Cons(h, t) => Some(balance(front, t))
       case _ => None
     }
 
-  final def lastOption: Option[A] =
+  def lastOption: Option[A] =
     rear.headOption
 
-  final def tailOption: Option[Deque[A]] =
+  def map[B](f: A => B): Deque[B] =
+    new Deque(front.map(f), rear.map(f))
+
+  def tailOption: Option[Deque[A]] =
     front match {
-      case Cons(h, t) => Some(new Deque(t, rear).balance)
+      case Cons(h, t) => Some(balance(t, rear))
       case _ => None
     }
 
-  final def toList: List[A] = {
+  def toList: List[A] = {
     val r = rear.foldLeft(List.empty[A])((l, a) => a :: l)
     front.foldRight(r)((a, l) => a :: l)
   }
 
-  final def toStrictList: StrictList[A] =
+  def toStrictList: StrictList[A] =
     front ++ rear.reverse
 }
 
 object Deque {
+  import StrictList._
+
+  def apply[A](as: A*): Deque[A] = ???
+
+  private def balance[A](front: StrictList[A], rear: StrictList[A]): Deque[A] =
+    (front, rear) match {
+      case (Nil(), Nil()) => empty
+      case (f, Nil()) =>
+        val (nf, nr) = f.halve
+        new Deque(nf, nr.reverse)
+      case (Nil(), r) =>
+        val (nr, nf) = r.halve
+        new Deque(nf.reverse, nr)
+      case _ => new Deque(front, rear)
+    }
+
   def empty[A]: Deque[A] =
     new Deque[A](StrictList.empty, StrictList.empty)
 
   def fromSeq[A](as: Seq[A]): Deque[A] = ???
 
-  def fromStrictList[A](list: StrictList[A]): Deque[A] = {
-    val (front, rear) = list.halve
-    new Deque[A](front, rear.reverse)
-  }
+  def fromStrictList[A](list: StrictList[A]): Deque[A] =
+    balance(list, StrictList.empty)
 }
